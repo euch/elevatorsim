67e78c8adccf1fdabfc02b3b799f28ff9df847b4
euch <euch@mail>
2022-07-10 14:31:52 +0400

    Winch Actor + Test, logging, formatting

--- src/main/scala/org/euch/elevatorsim/domain/model/shaft/BaseShaft.scala
+++ src/main/scala/org/euch/elevatorsim/domain/model/shaft/BaseShaft.scala
@@ -7,11 +7,13 @@ import org.euch.elevatorsim.domain.model.portal.Portal
 import org.euch.elevatorsim.domain.model.transport.Transport
 import org.euch.elevatorsim.domain.model.winch.Winch
 
-
-case class BaseShaft(override val portals: List[Portal],
-                       override val name: String,
-                       override val dimensions: DimensionsBox,
-                       override val transport: Transport,
-                       override val winch: Winch) extends Shaft {
-  override val safetyRecords: List[SafetyRecord] = portals.flatMap(_.safetyRecords)
+case class BaseShaft(
+    override val portals: List[Portal],
+    override val name: String,
+    override val dimensions: DimensionsBox,
+    override val transport: Transport,
+    override val winch: Winch
+) extends Shaft {
+  override val safetyRecords: List[SafetyRecord] =
+    portals.flatMap(_.safetyRecords)
 }
---
+++ src/test/scala/org/euch/elevatorsim/simulation/winch/SingleSpeedWinchFsmSpec.scala
@@ -0,0 +1,58 @@
+package org.euch.elevatorsim.simulation.winch
+
+import akka.actor.testkit.typed.scaladsl.{LogCapturing, ScalaTestWithActorTestKit, TestProbe}
+import org.euch.elevatorsim.domain.model.dimensions.DimensionsRectangle
+import org.euch.elevatorsim.domain.model.winch.*
+import org.euch.elevatorsim.simulation.winch.{WinchActor, WinchCommand}
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.{AnyWordSpec, AnyWordSpecLike}
+
+import java.time.Instant
+
+class SingleSpeedWinchFsmSpec
+    extends ScalaTestWithActorTestKit
+    with AnyWordSpecLike {
+
+  "SingleSpeedWinchFsmSpec" must {
+    "work" in {
+      val winch =
+        SingleSpeedWinch(1, 2, 50, "Winch", DimensionsRectangle(50, 50))
+      val winchActor = spawn(WinchActor(winch))
+      val probe = TestProbe[Double]()
+      winchActor ! WinchCommand.GetSpeed(Instant.now(), probe.ref)
+      probe.expectMessage(0) // initial speed = 0
+
+      { // This type of winch gains speed immediately
+        val now = Instant.now()
+        winchActor ! WinchCommand.MoveCommand.GoUp(now)
+        winchActor ! WinchCommand.GetSpeed(now, probe.ref)
+        probe.expectMessage(1)
+      }
+
+      { // This type of winch slows down immediately
+        val now = Instant.now()
+        winchActor ! WinchCommand.Stop(now)
+        winchActor ! WinchCommand.GetSpeed(now, probe.ref)
+        probe.expectMessage(0)
+      }
+
+      { // This type of winch does not reverse without 'Stop' command
+        val now = Instant.now()
+        winchActor ! WinchCommand.MoveCommand.GoDown(now)
+        winchActor ! WinchCommand.MoveCommand.GoUp(now)
+        winchActor ! WinchCommand.GetSpeed(now, probe.ref)
+        probe.expectMessage(2)
+      }
+
+      { // Check reverse with 'Stop' command
+        val now = Instant.now()
+        winchActor ! WinchCommand.MoveCommand.GoDown(now)
+        winchActor ! WinchCommand.Stop(now)
+        winchActor ! WinchCommand.MoveCommand.GoUp(now)
+        winchActor ! WinchCommand.GetSpeed(now, probe.ref)
+        probe.expectMessage(1)
+      }
+    }
+  }
+}
